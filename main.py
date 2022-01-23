a4ffdce748ca86ae40fd02ac632f4481920133aa
Vait <vait.dark@yandex.ru>
2022-01-23 21:14:56 +0300

    version_0.2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from tkinter import N
 import numpy as np
 
-code = '010100'
+
 
 def generative_mat(N, P):
     mat=np.eye(N)#единичная матрица NxN
@@ -8,5 +9,24 @@ def generative_mat(N, P):
     generate_mat=np.concatenate((mat,chet_mat),axis=1)
     return generate_mat
 
-G = generative_mat(len(code), len(code))
-print(G)
+def convert_code(loc_code):
+    int(loc_code)
+    loc_code=list(map(int, loc_code)) #превращение сообщения в матрицу
+    np.matrix(loc_code)
+    return loc_code
+
+massege = input("Введите сообщение ")
+for code in  massege:
+    code=format(ord(code), 'b')
+    print(code)
+    code=str(code)
+
+
+    G = generative_mat(len(code), len(code))
+    print(G)
+    code=convert_code(code)
+    print(code)
+    code_mat=np.dot(code, G)
+    code_mat%=2
+    print(code_mat)
+    print()
