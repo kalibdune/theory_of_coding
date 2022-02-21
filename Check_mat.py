fcda51f3078cb5e0cd5dac3918a4a14680047c26
dmitry <kalibdune@yandex.ru>
2022-02-21 17:37:53 +0300

    измененно Hameng.py и Check_mat.py

--- Check_mat.py
+++ Check_mat.py
@@ -12,3 +12,17 @@ def convert_code(loc_code):
     loc_code=list(map(int, loc_code)) #превращение сообщения в матрицу
     np.matrix(loc_code)
     return loc_code
+
+def chet_mat(text):
+    result = ''
+    for code in text:
+        code=format(ord(code), 'b')
+        code=str(code)
+
+        G = generative_mat(len(code), len(code))
+        code=convert_code(code)
+        code_mat=np.dot(code, G)
+        code_mat%=2
+        result += str(code_mat) + '\n'
+    result = result.replace('.', '').replace('[', '').replace(']', '').replace(' ', '')#to string
+    return result
--- Hameng.py
+++ Hameng.py
@@ -1,39 +1,36 @@
 
-def hem(char):
-    code=format(ord(char), 'b')
-    code_word=list(code)
-    print(code_word)
-    for n in range(0,len(code_word)):
-        bin=code_word[n]
-        bin=int(bin)
-        code_word[n]=bin
-    print(code_word)
-    
-    k=0
-    for n in range(1,10):
-        if 2**n>=len(code_word)+n+1:
-            k=n
-            break
-    
-    for n in range(0,k):
-        code_word.insert(2**n-1, 0)
-        print(code_word)
-    
-    leen=len(code_word)-1
-    for n in range(0,k):
-        check_dec=0
-        for i in range(2**n-1,len(code_word), 2**n*2):
-            for j in range(0, 2**n):
-                if (i+j)<=leen:
-                    check_dec+=code_word[i+j]
-        code_word[2**n-1]=check_dec%2
+def hamming(text):
+    result = ''
+    for word in text:
+        code=format(ord(word), 'b')
+        code_word=list(code)
+        for n in range(0,len(code_word)):
+            bin=code_word[n]
+            bin=int(bin)
+            code_word[n]=bin
         
-    print(code_word)
-    return code_word
-def decod_hem():
-    code=[1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
+        k=0
+        for n in range(1,10):
+            if 2**n>=len(code_word)+n+1:
+                k=n
+                break
+        
+        for n in range(0,k):
+            code_word.insert(2**n-1, 0)
+        
+        leen=len(code_word)-1
+        for n in range(0,k):
+            check_dec=0
+            for i in range(2**n-1,len(code_word), 2**n*2):
+                for j in range(0, 2**n):
+                    if (i+j)<=leen:
+                        check_dec+=code_word[i+j]
+            code_word[2**n-1]=check_dec%2
+        
+        result += ' '.join(map(str, code_word)).replace(' ', '') + '\n'#to string
+    return result
 
-
+def decod_hamming(code):
     k=0
     while True:
         if 2**k>=len(code)+k+1:
@@ -51,7 +48,6 @@ def decod_hem():
                         check_dec+=code[i+j]
             arrk[n]=check_dec%2
 
-    print(arrk)
     for n in range(0,k):
         check_dec=0
         for i in range(2**n-1,len(code), 2**n*2):
@@ -61,4 +57,4 @@ def decod_hem():
                     arrk[n]=0
                 else:
                     arrk[n]=1
-    print(arrk)
+    return arrk
