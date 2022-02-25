42dbb2112c1bdcf76d2ae4624a834ad6d3760ea2
dmitry <kalibdune@yandex.ru>
2022-02-25 13:47:29 +0300

    изменения хамминг

--- hamming.py
+++ hamming.py
@@ -11,16 +11,20 @@ class Haming_method(QWidget):
         super(Haming_method, self).__init__(*args, **kwargs)
         uic.loadUi('ui/Haming_method.ui', self)
 
+        self.coding_button_pressed = False
+
         self.coding.clicked.connect(self.coding_button_handler)
         self.no_coding.clicked.connect(self.no_coding_button_handler)
         self.error_button.clicked.connect(self.error_button_handler)
         self.clear_button.clicked.connect(self.clear)
 
     def coding_button_handler(self):
+        self.coding_button_pressed = True
         text = self.user_text.toPlainText()
-        self.binary_text.setPlainText(hamming(text))
+        self.binary_text.setPlainText(hamming(text.strip()))
 
     def no_coding_button_handler(self):
+        self.coding_button_pressed = False
         text = self.user_text.toPlainText()
         self.binary_text.setPlainText(binary_code(text))
     
@@ -28,11 +32,16 @@ class Haming_method(QWidget):
         text = self.binary_text.toPlainText()
         error_text = random_error(text)
         self.error_text.setHtml(error_text)
-    
+        if self.coding_button_pressed == True:
+            self.decoded_text.setHtml('')
+        else:
+            self.decoded_text.setHtml('')
+
     def clear(self):
         self.user_text.clear()
         self.binary_text.clear()
         self.error_text.clear()
+        self.decoded_text.clear()
 
 if __name__ == '__main__':
     app = QApplication([])
